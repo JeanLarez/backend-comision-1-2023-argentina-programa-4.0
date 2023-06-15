f7569d39140c0065dfd34fc89073f765c2221beb
JeanLarez <jeanlarez@gmail.com>
2023-06-14 23:09:54 -0300

    add rutas-dinamicas-web-server

---
+++ rutas-dinamicas-web-server/cursos.js
@@ -0,0 +1,11 @@
+const cursos = [
+  { id: 1, nombre: "JavaScript", categoria: "Programación" }, // Objeto que representa un curso de JavaScript
+  { id: 2, nombre: "React JS", categoria: "Programación" }, // Objeto que representa un curso de React JS
+  { id: 3, nombre: "Vue", categoria: "Programación" }, // Objeto que representa un curso de Vue
+  { id: 4, nombre: "SQL", categoria: "Datos" }, // Objeto que representa un curso de SQL
+  { id: 5, nombre: "MongoDB", categoria: "Datos" }, // Objeto que representa un curso de MongoDB
+  { id: 6, nombre: "Ecommerce", categoria: "Productos" }, // Objeto que representa un curso de Ecommerce
+  { id: 7, nombre: "Costumer Experience", categoria: "Productos" }, // Objeto que representa un curso de Customer Experience
+];
+
+module.exports = cursos; // Exporta la variable "cursos" para que esté disponible en otros archivos que importen este módulo
---
+++ rutas-dinamicas-web-server/index.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const cursos = require("./cursos.js");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bienvenidos al servidor web con rutas dinámicas");
+});
+
+app.get("/cursos/:categoria", (req, res) => {
+  //   console.log(req.params.categoria.trim().toLowerCase()); // Imprime la categoría de cursos recibida en la URL
+  let parametro = req.params.categoria.trim().toLowerCase(); // Obtiene y normaliza la categoría de cursos recibida en la URL
+  if (parametro !== "") {
+    let resultado = [];
+    for (let curso of cursos) {
+      if (curso.categoria.toLowerCase() === parametro) {
+        resultado.push(curso);
+      }
+    }
+    resultado.length > 0
+      ? res.json(resultado) // Si se encontraron cursos que coinciden con la categoría, los envía como respuesta en formato JSON
+      : res.json([
+          { id: "Error", descripcion: "No se encontraron coincidencias." },
+        ]); // Si no se encontraron cursos que coinciden con la categoría, envía un mensaje de error en formato JSON
+  }
+});
+
+app.get("/cursos", (req, res) => {
+  let respuesta = [];
+  const queryParams = Object.keys(req.query);
+  if (queryParams.length === 0) {
+    console.log("No llegan parámetros. Envío el set de datos");
+    res.json({ id: "Error", descripcion: "No se recibieron parametros." });
+  } else {
+    console.log("llegan los parámetros nombre y categoria");
+    for (let curso of cursos) {
+      if (
+        curso.nombre.toLowerCase().includes(req.query.nombre.toLowerCase()) &&
+        curso.categoria
+          .toLowerCase()
+          .includes(req.query.categoria.toLowerCase())
+      ) {
+        respuesta.push(curso);
+      }
+    }
+  }
+  res.send(respuesta);
+
+  //otra opción:
+  //   let respuesta = [];
+  //   for (let curso of cursos) {
+  //     if (
+  //       curso.nombre.toLowerCase() === req.query.nombre.toLowerCase() &&
+  //       curso.categoria.toLowerCase() === req.query.categoria.toLowerCase()
+  //     ) {
+  //       respuesta.push(curso);
+  //     }
+  //   }
+  //   respuesta.length === 0 ? res.json(cursos) : res.json(respuesta);
+});
+
+app.get("/curso/codigo/:id", (req, res) => {
+  const codigo = parseInt(req.params.id);
+  let resultado = [];
+  if (typeof codigo === "number") {
+    for (let curso of cursos) {
+      if (curso.id === codigo) {
+        resultado.push(curso);
+        return res.json(resultado); // Devuelve el resultado encontrado como respuesta en formato JSON y finaliza la ejecución de la función
+      }
+    }
+  }
+  res.json([{ id: "Error", descripcion: "No se encontraron coincidencias." }]); // Si no se encontró un resultado, devuelve un mensaje de error en formato JSON
+});
+
+app.get("/curso/nombre/:nombre", (req, res) => {
+  const nombre = req.params.nombre.toLowerCase();
+  let resultado = [];
+  for (let curso of cursos) {
+    if (curso.nombre.toLowerCase().includes(nombre)) {
+      resultado.push(curso);
+    }
+  }
+  resultado.length > 0
+    ? res.json(resultado) // Si se encontraron cursos que coinciden con el nombre, los envía como respuesta en formato JSON
+    : res.json([
+        { id: "Error", descripcion: "No se encontraron coincidencias." },
+      ]); // Si no se encontraron cursos que coinciden con el nombre, envía un mensaje de error en formato JSON
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto local ${PORT}`);
+});
