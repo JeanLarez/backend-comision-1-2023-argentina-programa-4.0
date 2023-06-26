5e018fb5fd0973195d790b4b89c6caecaff5c504
JeanLarez <jeanlarez@gmail.com>
2023-06-26 11:55:53 -0300

    add exercise api rest full - json

--- api-rest-full-json/server.js
+++ api-rest-full-json/server.js
@@ -1,33 +1,47 @@
+// Cargar las variables de entorno del archivo .env
 require("dotenv").config();
+
+// Importar el módulo Express
 const express = require("express");
 const app = express();
+
+// Importar las funciones del gestor de frutas
 const { leerFrutas, guardarFrutas } = require("./src/frutasManager");
+
+// Configurar el número de puerto para el servidor
 const PORT = process.env.PORT || 3000;
 
+// Crear un arreglo vacío para almacenar los datos de las frutas
 let BD = [];
 
+// Configurar el middleware para analizar el cuerpo de las solicitudes como JSON
 app.use(express.json());
 
+// Middleware para leer los datos de las frutas antes de cada solicitud
 app.use((req, res, next) => {
-  BD = leerFrutas();
-  next();
+  BD = leerFrutas(); // Leer los datos de las frutas desde el archivo
+  next(); // Pasar al siguiente middleware o ruta
 });
 
+// Ruta principal que devuelve los datos de las frutas
 app.get("/", (req, res) => {
    res.send(BD);
 });
 
+// Ruta para agregar una nueva fruta al arreglo y guardar los cambios
 app.post("/", (req, res) => {
     const nuevaFruta = req.body;
-    BD.push(nuevaFruta);
-    guardarFrutas(BD);
-    res.status(201).send("Fruta agregada!");
+    BD.push(nuevaFruta); // Agregar la nueva fruta al arreglo
+    guardarFrutas(BD); // Guardar los cambios en el archivo
+    res.status(201).send("Fruta agregada!"); // Enviar una respuesta exitosa
 });
 
+// Ruta para manejar las solicitudes a rutas no existentes
 app.get("*", (req, res) => {
   res.status(404).send("Lo sentimos, la página que buscas no existe.");
 });
 
+// Iniciar el servidor y escuchar en el puerto especificado
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
--- api-rest-full-json/src/frutasManager.js
+++ api-rest-full-json/src/frutasManager.js
@@ -1,17 +1,20 @@
 const fs = require("fs");
-require('dotenv').config();
+require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
 
+// Función para guardar frutas en el archivo de base de datos
 function guardarFrutas(frutas) {
-  const datos = JSON.stringify(frutas);
-  fs.writeFileSync(__dirname + process.env.DATABASE_PATH, datos);
+  const datos = JSON.stringify(frutas); // Convertir el arreglo de frutas a formato JSON
+  fs.writeFileSync(__dirname + process.env.DATABASE_PATH, datos); // Escribir los datos en el archivo definido en la variable de entorno DATABASE_PATH
 }
 
+// Función para leer frutas desde el archivo de base de datos
 function leerFrutas() {
-  const frutasString = fs.readFileSync(__dirname + process.env.DATABASE_PATH, "utf8");
-  const frutas = JSON.parse(frutasString);
-  return frutas;
+  const frutasString = fs.readFileSync(__dirname + process.env.DATABASE_PATH, "utf8"); // Leer los datos del archivo definido en la variable de entorno DATABASE_PATH como una cadena de texto
+  const frutas = JSON.parse(frutasString); // Convertir la cadena de texto JSON a un arreglo de frutas
+  return frutas; // Devolver el arreglo de frutas
 }
 
+// Exportar las funciones para ser utilizadas por otros módulos
 module.exports = {
   leerFrutas,
   guardarFrutas,
